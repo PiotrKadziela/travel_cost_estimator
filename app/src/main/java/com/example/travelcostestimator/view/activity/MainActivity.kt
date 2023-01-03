70e14a537cba3e05f1efe3116fa824eaba97de6d
pkadziela <piotr.kadziela@lppsa.com>
2023-01-03 15:57:39 +0100

    BD-2 - Add trip list

--- app/src/main/java/com/example/travelcostestimator/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/travelcostestimator/view/activity/MainActivity.kt
@@ -2,13 +2,38 @@ package com.example.travelcostestimator.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.view.WindowManager
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.example.travelcostestimator.R
+import com.example.travelcostestimator.databinding.ActivityMainBinding
+import com.example.travelcostestimator.view.adapter.TripAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var tripAdapter: TripAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tripAdapter = TripAdapter(arrayListOf("Canada"))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvTripList.apply {
+            layoutManager = GridLayoutManager(
+                this@MainActivity,
+                1,
+                GridLayoutManager.VERTICAL,
+                false
+            )
+            adapter = tripAdapter
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/travelcostestimator/view/adapter/TripAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.travelcostestimator.view.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.travelcostestimator.databinding.TripListItemBinding
+
+class TripAdapter(private val trips: ArrayList<String>) : RecyclerView.Adapter<TripAdapter.TripViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TripViewHolder {
+        return TripViewHolder(TripListItemBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun onBindViewHolder(holder: TripViewHolder, position: Int) {
+        holder.binding.tvDestination.text = trips[position]
+    }
+
+    override fun getItemCount(): Int {
+        return trips.size
+    }
+
+    class TripViewHolder(val binding: TripListItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+}
