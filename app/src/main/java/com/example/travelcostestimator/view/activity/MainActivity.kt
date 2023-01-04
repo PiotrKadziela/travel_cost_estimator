00e4bff8f53b08106aa1601aedc24f931f4083a7
pkadziela <piotr.kadziela@lppsa.com>
2023-01-04 12:09:49 +0100

    BD-2 - Fix trip list item and add add button

--- app/src/main/java/com/example/travelcostestimator/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/travelcostestimator/view/activity/MainActivity.kt
@@ -2,16 +2,13 @@ package com.example.travelcostestimator.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.bumptech.glide.Glide
 import com.example.travelcostestimator.R
 import com.example.travelcostestimator.databinding.ActivityMainBinding
 import com.example.travelcostestimator.view.adapter.TripAdapter
+import java.lang.System
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,19 +18,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tripAdapter = TripAdapter(arrayListOf("Canada"))
+        tripAdapter = if (System.currentTimeMillis().toInt() % 2 == 0) {
+            TripAdapter(arrayListOf("Twój stary"))
+        } else {
+            TripAdapter(arrayListOf("Twój stary"))
+        }
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.rvTripList.apply {
-            layoutManager = GridLayoutManager(
-                this@MainActivity,
-                1,
-                GridLayoutManager.VERTICAL,
-                false
-            )
+            layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = tripAdapter
         }
 
+        if (tripAdapter.itemCount == 0) {
+            binding.rvTripList.visibility = View.INVISIBLE
+            binding.tvNoDataMessage.visibility = View.VISIBLE
+        }
 
     }
 }
--- app/src/main/java/com/example/travelcostestimator/view/adapter/TripAdapter.kt
+++ app/src/main/java/com/example/travelcostestimator/view/adapter/TripAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.travelcostestimator.view.adapter
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
