7710d94b76f7c540b9a87a75fd31980ef780f40a
pkadziela <piotr.kadziela@lppsa.com>
2023-01-06 12:41:27 +0100

    BD-3 - Add NewTripActivity

---
+++ app/src/main/java/com/example/travelcostestimator/view/activity/NewTripActivity.kt
@@ -0,0 +1,12 @@
+package com.example.travelcostestimator.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.travelcostestimator.R
+
+class NewTripActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_trip)
+    }
+}
